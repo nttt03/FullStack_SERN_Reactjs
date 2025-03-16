d9ce09f78748404a48adb43ad7e863a24d790d06
nttt03 <thanhthao03bd@gmail.com>
2025-03-16 22:03:02 +0700

    75_Create base component selection of data schedules and data loading...

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -4,6 +4,7 @@ import HomeHeader from '../../HomePage/HomeHeader';
 import './DetailDoctor.scss';
 import { getDetailInforDoctor } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
+import DoctorSchedule from './DoctorSchedule';
 
 class DetailDoctor extends Component {
 
@@ -15,11 +16,11 @@ class DetailDoctor extends Component {
     }
 
     async componentDidMount() {
-        if(this.props.match && this.props.match.params && this.props.match.params.id) {
+        if (this.props.match && this.props.match.params && this.props.match.params.id) {
             let id = this.props.match.params.id;
             let res = await getDetailInforDoctor(id);
-            if(res && res.errCode === 0) {
-                this.setState ({
+            if (res && res.errCode === 0) {
+                this.setState({
                     detailDoctor: res.data
                 })
             }
@@ -37,46 +38,53 @@ class DetailDoctor extends Component {
         let { language } = this.props;
         let { detailDoctor } = this.state;
         let nameVi, nameEn = '';
-        if(detailDoctor && detailDoctor.positionData) {
+        if (detailDoctor && detailDoctor.positionData) {
             nameVi = `${detailDoctor.positionData.valueVi}, ${detailDoctor.lastName} ${detailDoctor.firstName}`;
             nameEn = `${detailDoctor.positionData.valueEn}, ${detailDoctor.firstName} ${detailDoctor.lastName}`;
         }
         return (
             <Fragment>
-                <HomeHeader isShowBanner={false}/>
+                <HomeHeader isShowBanner={false} />
                 <div className='doctor-detail-container'>
                     <div className='intro-doctor'>
-                        <div className='content-left' style={{backgroundImage: `url(${detailDoctor && detailDoctor.image ? detailDoctor.image : ''})`}}>
-                        
+                        <div className='content-left' style={{ backgroundImage: `url(${detailDoctor && detailDoctor.image ? detailDoctor.image : ''})` }}>
+
                         </div>
                         <div className='content-right'>
                             <div className='up'>
                                 {language === LANGUAGES.VI ? nameVi : nameEn}
                             </div>
                             <div className='down'>
-                                { detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.description &&
+                                {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.description &&
                                     <span>
                                         {detailDoctor.Markdown.description}
                                     </span>
                                 }
                             </div>
                         </div>
-                    </div>   
-                    
+                    </div>
+
                     <div className='schedule-doctor'>
-                            
+                        <div className='content-left'>
+                            <DoctorSchedule
+                                doctorIdFromParent={detailDoctor && detailDoctor.id ? detailDoctor.id : -1}
+                            />
+                        </div>
+                        <div className='content-right'>
+
+                        </div>
                     </div>
                     <div className='detail-intro-doctor'>
                         {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML &&
-                            <div dangerouslySetInnerHTML={{__html: detailDoctor.Markdown.contentHTML}}>
-                                
+                            <div dangerouslySetInnerHTML={{ __html: detailDoctor.Markdown.contentHTML }}>
+
                             </div>
                         }
                     </div>
                     <div className='comment-doctor'>
-                            
+
                     </div>
-                </div>            
+                </div>
             </Fragment>
         );
     }
